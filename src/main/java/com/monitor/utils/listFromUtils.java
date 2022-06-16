0418cc423e5e7b57684b9152f07d0e0bba93f94c
sjf7788 <13054276335@163.com>
2022-06-16 11:10:16 +0800

    commit

---
+++ src/main/java/com/monitor/utils/listFromUtils.java
@@ -0,0 +1,50 @@
+package com.monitorList.utils;
+
+/**
+ * @Author sunjf
+ * @Description 名单来源字典匹配工具
+ */
+public class listFromUtils {
+
+    /**
+     * 公安
+     */
+    public static final String PLICE = "5200501";
+    /**
+     * 工商总局
+     */
+    public static final String STATE_ADMINISTRATION_FOR_INDUSTRY_AND_COMMERCE = "3600501";
+    /**
+     * 高法
+     */
+    public static final String HIGHEST_COURT = "4070501";
+    /**
+     * 工信部
+     */
+    public static final String MIIT = "500501";
+    /**
+     * 中国支付清算协会
+     */
+    public static final String PCAC = "600501";
+    /**
+     * 银行卡清算
+     */
+    public static final String CARD_CLEAR = "700501";
+    /**
+     * 行内
+     */
+    public static final String INNER_BANK = "800501";
+    /**
+     * 行内
+     */
+    public static final String INNER_BANK_A = "A00501";
+    /**
+     * 其他
+     */
+    public static final String OTHERS = "900501";
+    /**
+     * 其他
+     */
+    public static final String OTHERS_A = "其他";
+
+}
---
+++ src/main/java/com/monitor/utils/listTypeUtils.java
@@ -0,0 +1,74 @@
+package com.monitorList.utils;
+
+/**
+ * @Author sunjf
+ * @Description 表单类型字典匹配工具
+ */
+public class listTypeUtils {
+
+    /**
+     * 涉案黑名单
+     */
+    public static final String CASE_BLACKLIST = "1";
+    /**
+     * 涉案可疑
+     */
+    public static final String CASE_DUBIOUS = "2";
+    /**
+     * 公安直连涉案可疑
+     */
+    public static final String CASE_DUBIOUS_COP = "3";
+    /**
+     * 出租出借售卖银行卡
+     */
+    public static final String RENT_BORROW_SALE_CARD = "4";
+    /**
+     * 严重违法失信企业
+     */
+    public static final String ILLEGA_UNPROMISE_COMPANY = "5";
+    /**
+     * 注册地、州不存在，虚假地址
+     */
+    public static final String ILLEGAL_REGISTRATION = "6";
+    /**
+     * 一人多账户
+     */
+    public static final String ONE_FOR_ACCOUNTS = "7";
+    /**
+     * 多用户一手机（绑定手机号不合理）
+     */
+    public static final String PHONE_BINDING = "8";
+    /**
+     * 大量转入转出
+     */
+    public static final String IN_AND_OUT = "9";
+    /**
+     * 六个月无交易
+     */
+    public static final String NOBUSINESS_IN_SIXMONTHS = "10";
+    /**
+     * 可疑交易
+     */
+    public static final String DUBIOUS_BUSINESS = "11";
+    /**
+     * 微盘及违规交易所开户控制
+     */
+    public static final String VP_AND_ILLEGAL_EXCHANGEER_CONTROL = "12";
+    /**
+     * 暂停非柜面业务
+     */
+    public static final String SUSPENSION_OF_NON_COUNTER_BUSINESS = "13";
+    /**
+     * 禁止换卡换折
+     */
+    public static final String NO_CARD_AND_PASSBOOK = "14";
+    /**
+     * 证件过期暂停非柜面业务
+     */
+    public static final String OVERDATE_SUSPENSION_OF_NON_COUNTER_BUSINESS = "15";
+    /**
+     * 云账户管控-无交易
+     */
+    public static final String CLOUD_ACCOUTS_CONTROL = "16";
+
+}
